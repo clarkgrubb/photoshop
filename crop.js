86a1891da1760beeb634cf8509065aac53040028
Clark Grubb <clarkgrubb@gmail.com>
2021-05-08 17:47:41 -0700

    add crop script too

--- crop.js
+++ crop.js
@@ -1,13 +1,13 @@
-// letterbox an image to be square
+// crop an image to be square
 
 var height = app.activeDocument.height;
 var width = app.activeDocument.width;
 
 if (height > width) {
   var delta = (height - width) / 2
-  app.activeDocument.crop([-delta, 0, width + delta, height])
+  app.activeDocument.crop([0, delta, width, height - delta])
 }
 else if (height < width) {
   var delta = (width - height) / 2
-  app.activeDocument.crop([0, -delta, width, height + delta])
+  app.activeDocument.crop([delta, 0, width - delta, height])
 }
